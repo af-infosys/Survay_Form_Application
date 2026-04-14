c9918bc704a10473890b8f1bbb26c519a49dcc38
Jatin Poriya <kiritporiya25@gmail.com>
2026-04-14 11:31:27 +0530

    image probem

--- src/components/ImageUploadSlot.jsx
+++ src/components/ImageUploadSlot.jsx
@@ -19,6 +19,7 @@ const ImageUploadSlot = ({ label, slotKey, formData, setFormData }) => {
       );
     } catch (err) {
       console.log("Error Loading Image: ", err);
+      setViewLink(formData[slotKey]);
     }
   }, [formData]);
 
